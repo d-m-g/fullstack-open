9e615c91eff4c8b2664abb0a60fcb8032e2f791c
Dmitrii Gorovoi <gorovoi.dmitrii@gmail.com>
2026-06-25 21:36:46 +0300

    2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,37 +1,4 @@
-const Header = props => <h1>{props.course}</h1>
-
-const Content = ({ parts }) => (
-  <div>
-    {parts.map(part => (
-      <Part key={part.id} part={part} />
-    ))}
-  </div>
-)
-
-const Part = props => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-)
-
-const Total = props => (
-  <p>
-    {" "}
-    <strong>Total of {props.total} exercises</strong>
-  </p>
-)
-
-const Course = ({ course }) => {
-  const exercises = course.parts.map(part => part.exercises)
-  const total = exercises.reduce((a, c) => a + c)
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total total={total} />
-    </div>
-  )
-}
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
